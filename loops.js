61c1aaf180860688a0dd22e74131cf83dc5f44c2
alluring-machine-4767 <bgreiser@gmail.com>
2020-08-22 18:10:15 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
-function forLoop(x){
+function forLoop([x]){
   x = [];
-  for (let i = 0; i <= 25; i++) {
+  for (let i = 0; i < 25; i++) {
     if (i === 1) {
       x.push("I am 1 strange loop.")
     } else {
